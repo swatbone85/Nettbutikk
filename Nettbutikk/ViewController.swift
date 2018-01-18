648c0a33dfa86739c64e4efdd81d094f8033dff1
Thomas Swatland <thomasswatland@me.com>
2018-01-18 14:05:15 +0100

    Initial Commit

---
+++ Nettbutikk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nettbutikk
+//
+//  Created by Thomas Swatland on 18/01/2018.
+//  Copyright © 2018 Thomas Swatland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
